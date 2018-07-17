637c60fbde85643937b40577571bfa6593c69a11
quidcain <quidcain@gmail.com>
2018-07-17 03:08:16 +0300

    Initial commit

---
+++ src/app/main/sample/sample.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+
+import { FuseTranslationLoaderService } from '@fuse/services/translation-loader.service';
+
+import { locale as english } from './i18n/en';
+import { FuseConfigService } from '@fuse/services/config.service';
+
+@Component({
+    selector   : 'sample',
+    templateUrl: './sample.component.html',
+    styleUrls  : ['./sample.component.scss']
+})
+export class SampleComponent
+{
+    /**
+     * Constructor
+     *
+     * @param {FuseTranslationLoaderService} _fuseTranslationLoaderService
+     * @param _fuseConfigService
+     */
+    constructor(
+        private _fuseTranslationLoaderService: FuseTranslationLoaderService,
+        private _fuseConfigService: FuseConfigService
+    )
+    {
+        this._fuseTranslationLoaderService.loadTranslations(english);
+        // Configure the layout
+        this._fuseConfigService.config = {
+            layout: {
+                navbar   : {
+                    hidden: true
+                },
+                toolbar  : {
+                    hidden: true
+                },
+                footer   : {
+                    hidden: true
+                },
+                sidepanel: {
+                    hidden: true
+                }
+            }
+        };
+    }
+}
